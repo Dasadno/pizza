e33c7d6a02d7ff22a1a0e9ba81c1b32430f4393f
Dasadno <Yakaleks96@gmail.com>
2024-09-20 02:12:19 +0500

    Добавьте файлы проекта.

---
+++ arrtest/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+
+	const int arr_lenght = 4;
+	int list[arr_lenght];
+	int i{};
+
+	for (int i = 0; i < arr_lenght; i++)
+	{
+		std::cout << "¬ведите число в массив: ";
+		std::cin >> list[i];
+	}
+	
+	for(i = 0; i < arr_lenght; i++)
+	{
+		std::cout << "list[" << i + 1 << "] = " << list[i] << "\n";
+	}
+
+
+	return 0;
+}
